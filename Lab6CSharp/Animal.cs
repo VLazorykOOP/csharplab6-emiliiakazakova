76e338ea5903d36df5e67a56c26c74083f384e1f
emiliiakazakova <kazakova.emiliia@chnu.edu.ua>
2024-06-16 23:36:31 +0300

    Animal.cs

---
+++ Lab6CSharp/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab6CSharp
+{
+    class Animal : UseInterface
+    {
+        public string Name { get; set; }
+
+        public void DisplayUserInfo()
+        {
+            Console.WriteLine($"I am an animal. My name is {Name}");
+        }
+    }
+}
